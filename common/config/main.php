ddd5217998bfa74d8d634548b358e06ee55883f4
Lev <slarktiv34@gmail.com>
2023-03-23 14:35:09 +0300

    yii2 advanced

--- common/config/main.php
+++ common/config/main.php
@@ -2,12 +2,25 @@
 return [
     'aliases' => [
         '@bower' => '@vendor/bower-asset',
-        '@npm'   => '@vendor/npm-asset',
+        '@npm' => '@vendor/npm-asset',
     ],
-    'vendorPath' => dirname(dirname(__DIR__)) . '/vendor',
+    'vendorPath' => dirname(__DIR__, 2) . '/vendor',
+    'modules' => [
+
+    ],
     'components' => [
         'cache' => [
             'class' => \yii\caching\FileCache::class,
+        ],
+        'urlManager' => [
+            'class' => yii\web\UrlManager::class,
+            'showScriptName' => false,
+            'enablePrettyUrl' => true,
+            'rules' => array(
+                '<controller:\w+>/<id:\d+>' => '<controller>/view',
+                '<controller:\w+>/<action:\w+>/<id:\d+>' => '<controller>/<action>',
+                '<controller:\w+>/<action:\w+>' => '<controller>/<action>',
+            ),
         ],
     ],
 ];
